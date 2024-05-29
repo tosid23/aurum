934bdb0153cc8277018cbecdb477ab0316787194
Sid <tosid23@gmail.com>
2024-05-29 08:46:34 +0530

    Moved to kotlin 2.0

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -29,6 +30,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
     }
 }
 
